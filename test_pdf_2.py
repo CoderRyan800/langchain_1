0f98309c46f97484dbdcd48819d5ac8e507e6ec7
Ryan Mukai <ryanmukai@pop-os.localdomain>
2023-08-13 20:06:10 -0700

    Assessment of unpublished 2020 paper

--- test_pdf_2.py
+++ test_pdf_2.py
@@ -19,8 +19,46 @@ query = "Please create a re-written abstract to help me get started."
 
 response = proc_obj.query_the_document(query)
 
-fp = open('paper_suggestions_v2.txt','w')
+fp = open('paper_suggestions_abstract.txt','w')
 
 fp.write(response['answer'])
+
+fp.close()
+
+query = "Please state the last two questions that I asked you?"
+
+response = proc_obj.query_the_document(query)
+
+fp = open('response_to_simple_question.txt','w')
+
+fp.write(response['answer'])
+
+fp.close()
+
+query = "Please provide a starter introduction to aid me in getting started."
+
+response = proc_obj.query_the_document(query)
+
+fp = open('paper_suggestions_intro.txt','w')
+
+fp.write(response['answer'])
+
+fp.close()
+
+query = "Is this research actually innovative, or is it only a rehash of what already exists?  I know you don't have knowledge beyond a certain date but please try to answer.  If this is innovative, what is the key innovation?  If it is not, what other literature already covers knowledge of own state in a neural network that makes this redundant?"
+
+response = proc_obj.query_the_document(query)
+
+fp = open('assessment_of_whether_this_is_innovative.txt','w')
+
+fp.write(response['answer'])
+
+fp.close()
+
+json_string = json.dumps(json.loads(proc_obj.memory.json()),indent=4)
+
+fp = open('memory.json','w')
+
+fp.write(json_string)
 
 fp.close()
